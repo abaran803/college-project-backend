865c153c734031f522586d75abaf5dc093107dff
ayush <abaran803@gmail.com>
2023-05-23 16:25:40 +0530

    improvement

--- public/config/dataConfig.js
+++ public/config/dataConfig.js
@@ -1,6 +1,6 @@
 // Configuring the Database here
 const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGO_URI)
-mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+mongoose.connect(process.env.mongodb_uri)
     .then(() => console.log('database connected'))
     .catch((error) => console.log('error', error.message))
